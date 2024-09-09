d9e82ae18230ef93899bfcc6f3b8d87d6f6e11b1
mehran <mhr.as1317@gmail.com>
2024-09-09 23:47:08 +0330

    add function send_mail

---
+++ pytide_courier/mail.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.message import EmailMessage
+from dotenv import load_dotenv
+import os
+
+def send_mail(address, subject, message):
+    load_dotenv()
+    EMAIL_USER = os.getenv('EMAIL_USER')
+    EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+    SMTP_SERVER = os.getenv('SMTP_SERVER')
+    SMTP_PORT = int(os.getenv('SMTP_PORT'))
+
+    msg = EmailMessage()
+    msg.set_content(message)
+    msg['Subject'] = subject
+    msg['From'] = EMAIL_USER
+    msg['To'] = address
+    print(msg)
+    server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    server.starttls()
+    server.login(EMAIL_USER, EMAIL_PASSWORD)
+    server.send_message(msg)
+    server.quit()
